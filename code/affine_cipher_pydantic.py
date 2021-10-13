95bea7dacb72932d48e572a1bea5ec888126fa7c
Stan <stanbergevoet@gmail.com>
2021-10-13 14:34:57 +0200

    Changed help on mode arg

--- code/affine_cipher_pydantic.py
+++ code/affine_cipher_pydantic.py
@@ -51,7 +51,7 @@ def parse_arguments():
                                      description="""An affine cipher encrypts a message with the following equation `(ax+b) % m`.
                                                     Decryption is done with `(a^-1)(x-b) % m`. `m` is the length of the alphabet used.""")
 
-    parser.add_argument('mode', help="<decrypt> | <encrypt>", type=str)
+    parser.add_argument('mode', help="decrypt | encrypt", type=str)
     parser.add_argument('a', type=int)
     parser.add_argument('b', type=int)
     parser.add_argument('message', type=str)
